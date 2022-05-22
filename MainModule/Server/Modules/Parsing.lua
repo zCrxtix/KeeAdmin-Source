d37ffb83f0a19221730e59d64ee5f68e04654fa8
zCrxtix <71601671+zCrxtix@users.noreply.github.com>
2022-05-22 12:18:11 -0500

    Create Parsing.lua

---
+++ MainModule/Server/Modules/Parsing.lua
@@ -0,0 +1,31 @@
+--// Parses the message so its easier to manage in the main script
+
+local parsing = {}
+
+function parsing.parse_arguments(msg : string)
+   local r = string.split(msg, " ")
+   parsing.command_name = r[1]  
+   return r
+end
+
+function parsing.remove_prefix(msg : string)
+    return string.sub(msg, 2)
+end
+
+function parsing.check_prefix(msg : string)
+    return string.sub(msg, 1, 1)
+end
+
+function parsing.lower_string(msg : string)
+    return string.lower(msg)
+end
+
+function parsing.get_command_name(msg : string)
+    return parsing.command_name
+end
+
+function parsing.split_command(msg : string, split : string)
+	return string.split(msg, split)
+end
+
+return parsing
